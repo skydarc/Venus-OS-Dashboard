ca01878bbd955d7bc0fe932cee309ceecd4130d9
skydarc <65732104+skydarc@users.noreply.github.com>
2026-02-27 09:42:49 +0100

    Add files via upload

---
+++ dist/lang-nl.js
@@ -0,0 +1,43 @@
+export default {
+  tab1Render: {
+    theme_choice: "Selecteer kaartthema:",
+    light: "Licht",
+    dark: "Donker",
+    auto: "Automatisch",
+    devices_per_column: "Aantal \"apparaten\" per kolom:",
+    font_size_zones: "Lettergrootte in \"apparaten\"-zones:",
+    in_header: "in koptekst",
+    in_devices: "in apparaten",
+    in_footer: "in voettekst",
+    font_size: "Lettergrootte",
+  },
+  subtabRender: {
+    header_title: "Configuratie koptekst",
+    icon_choice: "Pictogram kiezen",
+    name_choice: "Naam van apparaat",
+    sensor_title: "Hoofd-sensor configuratie",
+    entity_choice: "Kies entiteit",
+    entity2_choice: "Kies secundaire entiteit",
+    enable_graph: "Grafiek inschakelen",
+    enable_gauge: "Meter in % inschakelen",
+    warning_gauge: "Kan alleen worden ingeschakeld als de eenheid % is",
+    header_footer_title: "Configuratie van sensoren in koptekst en voettekst",
+    entity_header: "Selecteer koptekst-entiteit",
+    entity_footer: "Selecteer voettekst-entiteit",
+    entity2_footer: "Selecteer 2e voettekst-entiteit",
+    entity3_footer: "Selecteer 3e voettekst-entiteit",
+    anchor_title: "Anker toevoegen",
+    left_qtyBox: "Links",
+    top_qtyBox: "Boven",
+    bottom_qtyBox: "Onder",
+    right_qtyBox: "Rechts",
+    add_links: "Links toevoegen",
+    add_link: "Link toevoegen"
+  },
+  addLink: {
+    start: "Start",
+    end: "Einde",
+    entity_picker: "Selecteer entiteit",
+    reverse: "omkeren"
+  }
+};
---
+++ dist/lang-pt.js
@@ -0,0 +1,43 @@
+export default {
+  tab1Render: {
+    theme_choice: "Selecionar tema do cartão:",
+    light: "Claro",
+    dark: "Escuro",
+    auto: "Automático",
+    devices_per_column: "Número de \"Dispositivos\" por coluna:",
+    font_size_zones: "Tamanho da fonte nas zonas de \"Dispositivos\":",
+    in_header: "no cabeçalho",
+    in_devices: "nos dispositivos",
+    in_footer: "no rodapé",
+    font_size: "Tamanho da fonte",
+  },
+  subtabRender: {
+    header_title: "Configuração do cabeçalho",
+    icon_choice: "Escolher ícone",
+    name_choice: "Nome do dispositivo",
+    sensor_title: "Configuração do sensor principal",
+    entity_choice: "Escolher entidade",
+    entity2_choice: "Escolher entidade secundária",
+    enable_graph: "Ativar gráfico",
+    enable_gauge: "Ativar indicador em %",
+    warning_gauge: "Só pode ser ativado se a unidade de medida for %",
+    header_footer_title: "Configuração dos sensores de cabeçalho e rodapé",
+    entity_header: "Selecionar entidade do cabeçalho",
+    entity_footer: "Selecionar entidade do rodapé",
+    entity2_footer: "Selecionar 2ª entidade do rodapé",
+    entity3_footer: "Selecionar 3ª entidade do rodapé",
+    anchor_title: "Adicionar âncora",
+    left_qtyBox: "Esquerda",
+    top_qtyBox: "Cima",
+    bottom_qtyBox: "Baixo",
+    right_qtyBox: "Direita",
+    add_links: "Adicionar ligações",
+    add_link: "Adicionar ligação"
+  },
+  addLink: {
+    start: "Início",
+    end: "Fim",
+    entity_picker: "Selecionar entidade",
+    reverse: "inverter"
+  }
+};
